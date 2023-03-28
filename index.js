346d6113aba17c83dd914ba50e7bef14eae80c33
Halim Choi <hchoicareer@gmail.com>
2023-03-28 10:28:34 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(names, target) {
+    return names.filter(name => name.toLowerCase() === target.toLowerCase());
+}
+
+function fuzzyMatch(names, target) {
+    return names.filter(name => name.startsWith(target));
+}
+
+function matchName(drivers, target) {
+    return drivers.filter(driver => driver.name === target);
+}
